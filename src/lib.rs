51074f5b84292a6f2139c9e297b92c820cb587a0
Alain <alain.brenzikofer@scs.ch>
2019-03-26 16:07:48 +0100

    now metadata hex string is isolated

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,99 @@
-
+#![deny(warnings)]
 extern crate hyper;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
 
-use std::io::{self, Write};
+use hyper::rt::{self, Future, Stream};
 use hyper::{Client, Method, Request, Body};
 use hyper::header::HeaderValue;
-use hyper::rt::{self, Future, Stream};
-use serde_json::{Value, map::Map};
 
+pub fn doit() {
+    let url = "http://127.0.0.1:9933".parse().unwrap();
 
-
-pub fn getMetadata() {
-    rt::run(rt::lazy(|| {
-        // This is main future that the runtime will execute.
-        //
-        // The `lazy` is because we don't want any of this executing *right now*,
-        // but rather once the runtime has started up all its resources.
-        //
-        // This is where we will setup our HTTP client requests.
-        let client = Client::new();
-
-        let json = r#"{"method": "state_getMetadata", "params": null, "jsonrpc": "2.0", "id": 0}"#;
-        let uri: hyper::Uri = "http://127.0.0.1:9933".parse().unwrap();
-        let mut req = Request::new(Body::from(json));
-        *req.method_mut() = Method::POST;
-        *req.uri_mut() = uri.clone();
-        req.headers_mut().insert(
-            hyper::header::CONTENT_TYPE,
-            HeaderValue::from_static("application/json")
-        );
-        let post = client.request(req).and_then(|res| {
-            println!("POST: {}", res.status());
-
-            res.into_body().concat2()
+    let fut = fetch_json(url)
+        // use the parsed vector
+        .map(|ret| {
+            // print users
+            println!("result: {:#?}", ret.result);
+        })
+        // if there was an error print it
+        .map_err(|e| {
+            match e {
+                FetchError::Http(e) => eprintln!("http error: {}", e),
+                FetchError::Json(e) => eprintln!("json parsing error: {}", e),
+            }
         });
 
-        //let body = post.wait().unwrap();
-
-        
-        post
-            .map(|body| {
-                println!("metadata result: {:?}", body);
-                //let ret: &[u8] = serde_json::from_slice(&body).unwrap();
-
-                //println!("GET: {:?}", got);
-                
-                //println!("extracted: {:?}", metadata["result"]);
-                //Ok(body)
-            })
-            .map_err(|err| {
-                println!("Error: {}", err);
-            })
-    
-
-    }));
+    // Run the runtime with the future trying to fetch, parse and print json.
+    //
+    // Note that in more complicated use cases, the runtime should probably
+    // run on its own, and futures should just be spawned into it.
+    rt::run(fut);
 }
 
-// run local 'substrate --dev' node to run these tests
-#[test]
-fn test_getter() {
-    getMetadata();
+fn fetch_json(url: hyper::Uri) -> impl Future<Item=JsonResponse, Error=FetchError> {
+    let client = Client::new();
 
+    let json = r#"{"method": "state_getMetadata", "params": null, "jsonrpc": "2.0", "id": 0}"#;
+    let mut req = Request::new(Body::from(json));
+    *req.method_mut() = Method::POST;
+    *req.uri_mut() = url.clone();
+    req.headers_mut().insert(
+        hyper::header::CONTENT_TYPE,
+        HeaderValue::from_static("application/json")
+    );
+
+    client
+        // Fetch the url...
+        .request(req)
+        // And then, if we get a response back...
+        .and_then(|res| {
+            // asynchronously concatenate chunks of the body
+            res.into_body().concat2()
+        })
+        .from_err::<FetchError>()
+        // use the body after concatenation
+        .and_then(|body| {
+            // try to parse as json with serde_json
+            let ret = serde_json::from_slice(&body)?;
+
+            Ok(ret)
+        })
+        .from_err()
+}
+
+#[derive(Deserialize, Debug)]
+struct User {
+    id: i32,
+    name: String,
+}
+
+// Define a type so we can return multiple types of errors
+enum FetchError {
+    Http(hyper::Error),
+    Json(serde_json::Error),
+}
+
+impl From<hyper::Error> for FetchError {
+    fn from(err: hyper::Error) -> FetchError {
+        FetchError::Http(err)
+    }
+}
+
+impl From<serde_json::Error> for FetchError {
+    fn from(err: serde_json::Error) -> FetchError {
+        FetchError::Json(err)
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct JsonResponse {
+    jsonrpc: String,
+    result: String,
+}
+#[test]
+fn test_it(){
+    doit();
 }
